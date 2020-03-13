fe3c31ca0edb0624fca1f0ea0e8c683fa4094931
Neil <neil.mf.benin@gmail.com>
2020-03-13 09:07:04 +0100

    noice

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Ticket;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comment/{id}", name="comment")
+     * @Route("/comment/{ticket}", name="comment")
+     * @param Ticket $ticket
+     * @return Response
      */
-    public function index($id)
+    public function index(Ticket $ticket)
     {
+        var_dump($ticket);
         return $this->render('comment/index.html.twig', [
-            'controller_name' => $id,
         ]);
     }
 }
